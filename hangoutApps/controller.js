b092446b6217b843a72f043fa2c1bda05c0d4311
cristover-lopes <cristover-lopes@users.noreply.github.com>
2014-05-20 18:48:49 +0100

    Update controller.js

--- hangoutApps/controller.js
+++ hangoutApps/controller.js
@@ -42,7 +42,7 @@ function sendHelpMsg(){
   var userName = startData.PUSDERID;
   var studyName = startData.NAME;
   
-  var msg = 'My name is ' + userName + ' and I need help with Study: ' + studyName '.';
+  var msg = 'My name is ' + userName + ' and I need help with Study: ' + studyName + '.';
   
    gapi.hangout.data.sendMessage(msg);
 }
